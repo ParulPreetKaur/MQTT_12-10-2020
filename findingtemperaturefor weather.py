1b314d4280ba66195716e63b39411c12c2348c01
ParulPreetKaur <kaurparulpreet@gmail.com>
2020-11-20 02:03:56 -0500

    Changes as on 20-11-2020

--- findingtemperaturefor weather.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def findtemperature(string):
-    Intnumber=re.findall('\d+', string)
-    Floatnumber=re.findall("\d+\.\d+", string)
-    if (Floatnumber):
-        return(Floatnumber)
-    else:
-        return(Intnumber)
